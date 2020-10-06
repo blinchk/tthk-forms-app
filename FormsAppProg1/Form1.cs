512134ec965b0f30e98ed82799950106aed97d15
Nikolas Laus <49353043+blinchk@users.noreply.github.com>
2020-10-06 14:19:02 +0300

    Add form with treeView.

---
+++ FormsAppProg1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace FormsAppProg1
+{
+    public partial class Form1 : Form
+    {
+        private TreeView tree;
+        public Form1()
+        {
+            this.Height = 500;
+            this.Width = 600;
+            this.Text = "Vorm elementidega";
+            tree = new TreeView();
+            tree.Dock = DockStyle.Left;
+            tree.AfterSelect += TreeOnAfterSelect;
+            TreeNode tn = new TreeNode("Elemendid");
+            tn.Nodes.Add(new TreeNode("Nupp-Button"));
+            tree.Nodes.Add(tn);
+            this.Controls.Add(tree);
+        }
+
+        private void TreeOnAfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Text == "Nupp-Button")
+            {
+                Button btn = new Button();
+                btn.Text = "Vajuta siia";
+                btn.Location = new Point(50, 10);
+                btn.Height = 10;
+                btn.Width = 40;
+                this.Controls.Add(btn);
+            }
+        }
+    }
+}
